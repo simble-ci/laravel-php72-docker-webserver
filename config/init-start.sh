b585569aef20f7b13c49c02a841c0bf575af5331
Simble CI <tien@simble.io>
2019-07-01 14:42:10 +0700

    Update init-start.sh

--- config/init-start.sh
+++ config/init-start.sh
@@ -2,5 +2,6 @@
 export HOME=/home/jenkins
 service mysql start
 service redis-server start
+mysql -u root -e "use mysql;update user set authentication_string=password('1111') where user='root'"
 
 while true; do sleep 1d; done
