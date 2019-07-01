bd2a5c1e1bc7e6ff4a761953d43029d2ca1faa90
Simble CI <tien@simble.io>
2019-07-01 14:01:14 +0700

    Update init-start.sh

--- config/init-start.sh
+++ config/init-start.sh
@@ -3,4 +3,7 @@ export HOME=/home/jenkins
 service mysql start
 service redis-server start
 
+mysql -uroot -e "CREATE USER 'ga'@'localhost' IDENTIFIED BY 'acresta';"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'ga'@'localhost' WITH GRANT OPTION;"
+
 while true; do sleep 1d; done
